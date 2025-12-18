2438410f7a52421c6690bdf45db0413e164c7225
Megha <a.megha@ra-360.com>
2025-12-18 08:01:05 -0500

    Dec 17

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -29,6 +29,12 @@ export const topicColors: Record<Topic, string> = {
 // Add your posts here! Just copy a post object and modify it.
 export const posts: Post[] = [
   {
+    id: '3',
+    content: '<strong>Monotonic Stack </strong>: Maintain a stack that is always increasing or decreasing. When the order breaks, pop until it’s restored.</br><i>Time</i> : <strong>O(n)</strong>: each element pushed once, popped once. </br><i>Space</i> : <strong>O(n)</strong> : Stack holds all elements in the worst case.</br><i>Examples</i>: histogram area, stock span, and next greater element',
+    topic: 'dsa',
+    date: '2025-12-17',
+  },
+  {
     id: '2',
     content: '<strong>Back-of-the-envelope estimations </strong>: <ul> <li>Power of two</li> <li>Latency numbers: Avoid disk seek, compress data before sending over internet</li><li>Availability numbers: more the 9s after decimal point, the lesser downtime</li></ul>',
     topic: 'system-design',
@@ -36,7 +42,7 @@ export const posts: Post[] = [
   },
   {
     id: '1',
-    content: '<strong>Cost Function</strong>: It is used to tune your model paramaters <i>w</i> and <i>b</i> to get the best fit. It also uses contour map for visual understanding.',
+    content: '<strong>Cost Function</strong>: A cost function measures how wrong a model’s predictions are. Training is just the process of <strong>minimizing this function</strong>, not “making the model smart.” Choose the wrong cost function, and even a powerful model will learn the wrong behavior.',
     topic: 'ml',
     date: '2025-12-15',
   },
